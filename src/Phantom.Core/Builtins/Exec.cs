7d0d267721f2a14e8717f2d0a051c7ad9dd895e0
Nathan Palmer <nathan1@nathanpalmer.com>
2010-05-05 22:39:04 -0600

    * Added missing files from the last commit

---
+++ src/Phantom.Core/Builtins/Exec.cs
@@ -0,0 +1,153 @@
+﻿namespace Phantom.Core.Builtins {
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Threading;
+    using Boo.Lang;
+
+    public class Exec {
+        private StreamReader _stdError;
+        private StreamReader _stdOut;
+        private TextWriter _outputWriter;
+        private TextWriter _errorWriter;
+        private string _exeName;
+
+        /// <summary>
+        /// Will be used to ensure thread-safe operations.
+        /// </summary>
+        private object _lockObject = new object();
+        
+        public virtual FileInfo Output { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether output will be appended to the 
+        /// <see cref="Output" />.
+        /// </summary>
+        /// <value>
+        /// <see langword="true" /> if output should be appended to the <see cref="Output" />; 
+        /// otherwise, <see langword="false" />.
+        /// </value>
+        public virtual bool OutputAppend
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="TextWriter" /> to which standard output
+        /// messages of the external program will be written.
+        /// </summary>
+        /// <value>
+        /// The <see cref="TextWriter" /> to which standard output messages of 
+        /// the external program will be written.
+        /// </value>
+        /// <remarks>
+        /// By default, standard output messages wil be written to the build log
+        /// with level <see cref="Level.Info" />.
+        /// </remarks>
+        public virtual TextWriter OutputWriter
+        {
+            get
+            {
+                //if (_outputWriter == null)
+                //{
+                //    _outputWriter = new TextWriter();
+                //    _outputWriter = new LogWriter(this, Level.Info,
+                //        CultureInfo.InvariantCulture);
+                //}
+                return _outputWriter;
+            }
+            set { _outputWriter = value; }
+        }
+
+        /// <summary>
+        /// The name of the executable that should be used to launch the 
+        /// external program.
+        /// </summary>
+        /// <value>
+        /// The name of the executable that should be used to launch the external
+        /// program, or <see langword="null" /> if no name is specified.
+        /// </value>
+        /// <remarks>
+        /// If available, the configured value in the NAnt configuration
+        /// file will be used if no name is specified.
+        /// </remarks>
+        public virtual string ExeName
+        {
+            get { return _exeName; }
+            set { _exeName = value; }
+        }
+
+        public void Execute(string args, Hash options) {
+            Thread outputThread = null;
+
+            string workingDir = options.ObtainAndRemove("WorkingDir", ".");
+            bool ignoreNonZeroExitCode = options.ObtainAndRemove("IgnoreNonZeroExitCode", false);
+            var psi = new ProcessStartInfo(_exeName, args)
+            {
+                WorkingDirectory = workingDir,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+            var process = Process.Start(psi);
+            
+            outputThread = new Thread(StreamReaderThread_Output);
+
+            _stdOut = process.StandardOutput;
+
+            outputThread.Start();
+
+            process.WaitForExit();
+            
+            outputThread.Join(2000);
+
+            var exitCode = process.ExitCode;
+
+            if (exitCode != 0 && ignoreNonZeroExitCode == false)
+            {
+                var errortext = process.StandardError.ReadAllAsString();
+                throw new ExecutionFailedException(exitCode, errortext);
+            }
+        }
+
+
+        /// <summary>
+        /// Reads from the stream until the external program is ended.
+        /// </summary>
+        private void StreamReaderThread_Output()
+        {
+            StreamReader reader = _stdOut;
+            bool doAppend = false;// OutputAppend;
+
+            while (true)
+            {
+                string logContents = reader.ReadLine();
+                if (logContents == null)
+                {
+                    break;
+                }
+
+                // ensure only one thread writes to the log at any time
+                lock (_lockObject)
+                {
+                    if (Output != null)
+                    {
+                        StreamWriter writer = new StreamWriter(Output.FullName, doAppend);
+                        writer.WriteLine(logContents);
+                        doAppend = true;
+                        writer.Close();
+                    }
+                    else
+                    {
+                        //OutputWriter.WriteLine(logContents);
+                    }
+                }
+            }
+
+            //lock (_lockObject)
+            //{
+            //    OutputWriter.Flush();
+            //}
+        }
+    }
+}
---
+++ src/Phantom.Core/Builtins/XmlPoke.cs
@@ -0,0 +1,110 @@
+﻿#region License
+
+// Copyright Jeremy Skinner (http://www.jeremyskinner.co.uk) and Contributors
+// 
+// Licensed under the Microsoft Public License. You may
+// obtain a copy of the license at:
+// 
+// http://www.microsoft.com/opensource/licenses.mspx
+// 
+// By using this source code in any fashion, you are agreeing
+// to be bound by the terms of the Microsoft Public License.
+// 
+// You must not remove this notice, or any other, from this software.
+
+#endregion
+
+namespace Phantom.Core.Builtins {
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using System.Xml;
+
+    [CompilerGlobalScope]
+    public sealed class XmlPoke {
+        public static string xmlpeek(string filename, string xpath, string property) {
+            Console.WriteLine(string.Format("Peeking at '{0}' with XPath expression '{1}'.", filename, xpath));
+
+            if (!File.Exists(filename)) {
+                throw new FileNotFoundException(string.Format("The file {0} was not found", filename), filename);
+            }
+
+            XmlDocument document = LoadDocument(filename);
+            return GetNodeContents(xpath, document, 0);
+        }
+
+        /// <summary>
+        /// Loads an XML document from a file on disk.
+        /// </summary>
+        /// <param name="fileName">The file name of the file to load the XML document from.</param>
+        /// <returns>
+        /// A <see cref="XmlDocument">document</see> containing
+        /// the document object representing the file.
+        /// </returns>
+        private static XmlDocument LoadDocument(string fileName)
+        {
+            XmlDocument document = null;
+
+            document = new XmlDocument();
+            document.Load(fileName);
+            return document;            
+        }
+
+        /// <summary>
+        /// Gets the contents of the node specified by the XPath expression.
+        /// </summary>
+        /// <param name="xpath">The XPath expression used to determine which nodes to choose from.</param>
+        /// <param name="document">The XML document to select the nodes from.</param>
+        /// <param name="nodeIndex">The node index in the case where multiple nodes satisfy the expression.</param>
+        /// <returns>
+        /// The contents of the node specified by the XPath expression.
+        /// </returns>
+        private static string GetNodeContents(string xpath, XmlDocument document, int nodeIndex)
+        {
+            string contents = null;
+            XmlNodeList nodes;
+
+            try
+            {
+                XmlNamespaceManager nsMgr = new XmlNamespaceManager(document.NameTable);
+                //foreach (XmlNamespace xmlNamespace in Namespaces)
+                //{
+                //    if (xmlNamespace.IfDefined && !xmlNamespace.UnlessDefined)
+                //    {
+                //        nsMgr.AddNamespace(xmlNamespace.Prefix, xmlNamespace.Uri);
+                //    }
+                //}
+                nodes = document.SelectNodes(xpath, nsMgr);
+            }
+            catch (Exception ex)
+            {
+                //throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                //    ResourceUtils.GetString("NA1155"), xpath),
+                //    Location, ex);
+                throw;
+            }
+
+            if (nodes == null || nodes.Count == 0)
+            {
+                //throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                //    ResourceUtils.GetString("NA1156"), xpath),
+                //    Location);
+                throw new Exception("Node not found");
+            }
+
+            Console.WriteLine(string.Format("Found '{0}' nodes with the XPath expression '{1}'.", nodes.Count, xpath));
+
+            if (nodeIndex >= nodes.Count)
+            {
+                throw new Exception("Something");
+                //throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                //    ResourceUtils.GetString("NA1157"), nodeIndex), Location);
+            }
+
+            XmlNode selectedNode = nodes[nodeIndex];
+            contents = selectedNode.InnerXml;
+            return contents;
+        }
+    }
+}
